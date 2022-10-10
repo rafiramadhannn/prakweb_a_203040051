9babe749d1686fa7e75532d2dd02a4e075cc6cc5
Mochamad Rafi Ramadhan <75578010+rafiramadhannn@users.noreply.github.com>
2022-10-10 16:39:34 +0700

    Delete phpmvc directory

--- phpmvc/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define ('BASEURL', 'http//localhost/prakweb_a_203040051/phpmvc/public');
-
-//DB
-define('DB_HOST', 'localhost');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_NAME', '');
